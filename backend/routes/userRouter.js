0faa4d09ed37a861e0161f5bad8eb4587d5d84d4
mahdisouki <mahdisouki88@gmail.com>
2023-08-24 17:00:33 +0100

    new commit

---
+++ backend/routes/userRouter.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+const userCtrl = require ('../controllers/userCtrl')
+const auth = require('../middelware/auth')
+
+router.post('/register',userCtrl.register)
+router.post('/login',userCtrl.login)
+router.get('/logout',userCtrl.logout)
+router.get('/refresh_token',userCtrl.refreshToken)
+router.get('/infor', auth , userCtrl.getUser)
+router.get('/users' , userCtrl.getAllUseres)  
+router.delete('/deleteusers/:id' , userCtrl.deleteUser)  
+router.put('/updateUser/:id' ,  userCtrl.UpdateUser)
+
+
+
+
+module.exports = router
